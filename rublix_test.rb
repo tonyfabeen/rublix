98e30f9f750a5941934f77dde8f07f73922c32a2
Tony Fabeen <tony.fabeen@akatus.com>
2013-07-17 14:12:34 -0300

    Refactoring Tests

--- rublix_test.rb
+++ rublix_test.rb
@@ -1,10 +1,16 @@
 require 'rublix'
 require 'test/unit'
 
-class ProlixTest < Test::Unit::TestCase
+class RublixTest < Test::Unit::TestCase
 
   def test_lxc_config_path
-    assert_equal Rublix::LXC.config_path, "/var/lxc/uga"
+    lxc_config_path = "/var/lib/lxc"
+    assert_equal Rublix::LXC.config_path, lxc_config_path
+  end
+
+  def test_get_version
+    lxc_version_supported = "0.9.0"
+    assert_equal Rublix::LXC.version, lxc_version_supported
   end
 
   def test_new_container
