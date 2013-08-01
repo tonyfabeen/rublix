623aab2a2b5ec329b0801f123eeaeeb7d527e780
Tony Fabeen <tony.fabeen@akatus.com>
2013-08-01 14:28:05 -0300

    Validate if container name is filled.

--- api/app.rb
+++ api/app.rb
@@ -2,6 +2,12 @@ require 'bundler/setup'
 require 'rublix'
 Bundler.require(:default)
 
+def json_errors(messages)
+  raise "Messages must be an Array" unless messages.is_a?(Array)
+  error_hash = {:errors => messages}
+  error_hash.to_json
+end
+
 get '/lxc-config-path' do
   Rublix::LXC.config_path + "\n"
 end
@@ -13,6 +19,12 @@ end
 #curl -X POST -H "Content-Type: application/json" -d '{"name":"containerplus"}' http://localhost:6000/containers/create
 post '/containers/create' do
   container = JSON.parse(request.body.read)
+  if container['name'].nil?
+    status 422
+    return json_errors(["Name must be filled"])
+  end
+
+  status 200
   container_hash = {:name => container['name']}
   container_hash.to_json
 end
